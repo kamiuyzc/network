95cd04de0b0e710d9075c33d35b0f83375d4592b
zcyang <kamiu.yang@gmail.com>
2019-12-20 17:25:52 +0800

    test

--- basicc/05_array/556_7.c
+++ basicc/05_array/556_7.c
@@ -3,15 +3,15 @@
 int main(void)
 {
 	int i, j;
-    char str1[] = "10 + 20 = ";
-    sscanf(str1, "%d + %d = ", &i, &j); 
+	char str1[] = "10 + 20 = ";
+	sscanf(str1, "%d + %d = ", &i, &j); 
 	printf("result is :%d\n", i + j);
 
 	char str2[100];
 	sprintf(str2, "%d + %d = %d", i, j, i+j);
 	printf("the result is %s", str2);
 
-	
+
 	return 0;
 }
 
