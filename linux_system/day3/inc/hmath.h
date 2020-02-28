96a103a854eba2a138ef2ea6fe52bfeb500a8c92
zcyang <kamiu.yang@gmail.com>
2020-02-28 10:54:38 +0800

    add

---
+++ linux_system/day3/inc/hmath.h
@@ -0,0 +1,14 @@
+#ifndef _HMATH_H_
+
+#define _HMATH_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b);
+int min(int a, int b);
+int mul(int a, int b);
+int dive(int a, int b);
+
+#endif
+
